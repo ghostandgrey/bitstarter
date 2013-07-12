8f6d07858133d933e9b6bf8fdd1e1a401041b221
Peter Altosaar <peter.altosaar@gmail.com>
2013-07-12 03:24:50 +0000

    Trying again

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buff = fs.readFile('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+	response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
